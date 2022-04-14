8d04e058690c354d25e3747ecf913dc6b68b8c80
Khayal_Dev <82506368+Khayal-Dev@users.noreply.github.com>
2022-04-14 19:52:34 +0300

    switch.c
    Add switch in C

---
+++ switch.c
@@ -0,0 +1,23 @@
+switch(day_number) {
+    case 1:
+        printf("Saturday");
+        break;
+    case 2:
+        printf("Sunday");
+        break;
+    case 3:
+        printf("Monday");
+        break;
+    case 4:
+        printf("Tuesday");
+        break;
+    case 5:
+        printf("Wednesday");
+        break;
+    case 6:
+        printf("Thursday");
+        break;
+    case 7:
+        printf("Friday");
+        break;
+}
