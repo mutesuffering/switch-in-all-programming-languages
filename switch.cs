6526a647ff0e188dd45bf66b6b8767d4950166a6
Khayal_Dev <82506368+Khayal-Dev@users.noreply.github.com>
2022-04-14 20:06:30 +0300

    switch.cs
    Add switch in C#

---
+++ switch.cs
@@ -0,0 +1,23 @@
+switch (day_number) {
+    case 1:
+        System.Console.WriteLine("Saturday");
+        break;
+    case 2:
+        System.Console.WriteLine("Sunday");
+        break;
+    case 3:
+        System.Console.WriteLine("Monday");
+        break;
+    case 4:
+        System.Console.WriteLine("Tuesday");
+        break;
+    case 5:
+        System.Console.WriteLine("Wednesday");
+        break;
+    case 6:
+        System.Console.WriteLine("Thursday");
+        break;
+    case 7:
+        System.Console.WriteLine("Friday");
+        break;
+}
