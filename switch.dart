cffe7ac6ee3b85b9277895bd1610dc60f4064cfd
Khayal_Dev <82506368+Khayal-Dev@users.noreply.github.com>
2022-04-14 20:03:19 +0300

    switch.dart
    Add switch in Dart

---
+++ switch.dart
@@ -0,0 +1,30 @@
+switch(day_number) {
+    case 1: {
+        print("Saturday");
+    }
+    break;
+    case 2: {
+        print("Sunday");
+    }
+    break;
+    case 3: {
+        print("Monday");
+    }
+    break;
+    case 4: {
+        print("Tuesday");
+    }
+    break;
+    case 5: {
+        print("Wednesday");
+    }
+    break;
+    case 6: {
+        print("Thursday");
+    }
+    break;
+    case 7: {
+        print("Friday");
+    }
+    break;
+}
