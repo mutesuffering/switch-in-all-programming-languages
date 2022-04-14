79707cff076bc9e3c6f1d366e690cc0102b3df7f
Khayal_Dev <82506368+Khayal-Dev@users.noreply.github.com>
2022-04-14 19:47:45 +0300

    switch.java
    Add Switch in Java

---
+++ switch.java
@@ -0,0 +1,23 @@
+switch(day_number) {
+    case 1:
+        System.out.println("Saturday");
+        break;
+    case 2:
+        System.out.println("Sunday");
+        break;
+    case 3:
+        System.out.println("Monday");
+        break;
+    case 4:
+        System.out.println("Tuesday");
+        break;
+    case 5:
+        System.out.println("Wednesday");
+        break;
+    case 6:
+        System.out.println("Thursday");
+        break;
+    case 7:
+        System.out.println("Friday");
+        break;
+}
