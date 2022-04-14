cbc1d49d5859604a2743f741bfd9905e9938f5aa
Khayal_Dev <82506368+Khayal-Dev@users.noreply.github.com>
2022-04-14 19:58:08 +0300

    switch.cpp
    Add switch in C++

---
+++ switch.cpp
@@ -0,0 +1,23 @@
+switch(day_number) {
+    case 1:
+        cout << "Saturday" << endl;
+        break;
+    case 2:
+        cout << "Sunday" << endl;
+        break;
+    case 3:
+        cout << "Monday" << endl;
+        break;
+    case 4:
+        cout << "Tuesday" << endl;
+        break;
+    case 5:
+        cout << "Wednesday" << endl;
+        break;
+    case 6:
+        cout << "Thursday" << endl;
+        break;
+    case 7:
+        cout << "Friday" << endl;
+        break;
+}
