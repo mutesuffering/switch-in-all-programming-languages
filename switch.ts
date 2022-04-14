344fed5cd32c1c2898078de2fc7b6a59bd0fabf9
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-04-14 23:16:31 +0200

    Add TypeScript

---
+++ switch.ts
@@ -0,0 +1,25 @@
+let day_number: number;
+
+switch(day_number) {
+  case 1:
+    console.log("Saturday");
+    break;
+  case 2:
+    console.log("Sunday");
+    break;
+  case 3:
+    console.log("Monday");
+    break;
+  case 4:
+    console.log("Tuesday");
+    break;
+  case 5:
+    console.log("Wednesday");
+    break;
+  case 6:
+    console.log("Thursday");
+    break;
+  case 7:
+    console.log("Friday");
+    break;
+}
