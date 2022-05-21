4c6e1f614fd271c7e61936fa0afea5d31ae01d94
Abdelrahman Dwedar <dedou2005@gmail.com>
2022-05-21 11:40:09 +0200

    ➕ Add Ruby language
    In Ruby we don't have a normal switch statement, we use the case statement instead, wheich is pretty the same
    
    switch => case
    case => when
    default => else

---
+++ switch.rb
@@ -0,0 +1,16 @@
+case day_number
+  when 1
+    puts "Saturday"
+  when 2
+    puts "Sunday"
+  when 3
+    puts "Monday"
+  when 4
+    puts "Tuesday"
+  when 5
+    puts "Wednesday"
+  when 6
+    puts "Thursday"
+  when 7
+    puts "Friday"
+end
