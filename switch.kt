8aaf1e2ac6a3858c1b24eabd2e6c7a3af4bba0ac
Abdelrahman Dwedar <dedou2005@gmail.com>
2022-05-23 23:18:52 +0200

    ➕ Add Kotlin
    Add Kotlin using the when case, which is replaces the switch case in other languages.

---
+++ switch.kt
@@ -0,0 +1,13 @@
+fun main() {
+	val day_number: Int = 6;
+  
+ 	when (day_number) {
+ 	 	1 -> print("Saturday")
+ 	 	2 -> print("Sunday")
+	  	3 -> print("Monday")
+ 	 	4 -> print("Tuesday")
+	  	5 -> print("Wednesday")
+	  	6 -> print("Thursday")
+	  	7 -> print("Friday")
+	}
+}
