7ad74953c5dea6f1a4ff0941897755a14c6e66be
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-05-26 15:46:02 +0200

    :heavy_plus_sign: Add Rust

---
+++ switch.rs
@@ -0,0 +1,12 @@
+fn main() {
+    match day_number {
+        1 => println!("Saturday"),
+        2 => println!("Sunday"),
+        3 => println!("Monday"),
+        4 => println!("Tuesday"),
+        5 => println!("Wednesday"),
+        6 => println!("Thursday"),
+        7 => println!("Friday"),
+        _ => return
+    }
+}
